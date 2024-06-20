0a0d6f995723d117ad2faa711f983ce1bfa3ea8e
davewrld <keithdave23@gmail.com>
2024-06-20 16:15:56 -0700

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Function queries Reddit API
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Queries Reddit API, returns number of subscribers """
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = {'User-Agent': 'MyApp/1.0'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscriber_count = data["data"]["subscribers"]
+        return (subscriber_count)
+    else:
+        return (0)
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,15 @@
+API ADVANCED
+
+ Practise - Reddit API
+
+Learning Objectives
+How to read API documentation to find the endpoints you’re looking for
+How to use an API with pagination
+How to parse JSON results from an API
+How to make a recursive API call
+How to sort a dictionary by value
+
+Tasks
+0. FUNCTION THAT QUERIES THE REDDIT  API AND RETURNS THE NUMBER OF SUBSCRIBER.
+1. QUESRIES AND PRINTS TITLE OF THE FIRST 10 HOT POSTS
+2. RECURSIVE FUNCTION THAT QUERIES API RETURNS LIST OF ALL HT ARTICLES
