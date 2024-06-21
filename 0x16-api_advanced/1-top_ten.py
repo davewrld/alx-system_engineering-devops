4f7a91f572c848c0da854a708a0c7ca29d9b8f31
davewrld <keithdave23@gmail.com>
2024-06-20 17:11:51 -0700

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""prints the titles of the first 10 hot posts listed Reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """Function queries API"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "0x16-api_advanced/1.0"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data["data"]["children"]
+
+        for post in posts:
+            print(post["data"]["title"])
+    else:
+        print(None)
